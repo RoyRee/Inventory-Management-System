af32c6efbd9c8c6619b6eb349029f8ca27a97c87
RoyRee <rahulshrirame6@gmail.com>
2021-07-25 22:25:37 +0530

    Removed unnecessary file

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+// import './homepage.styles.css';
+import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+const Homepage =() =>(
+    <div className='homepage'>
+        <h1 class="text-center">Dashboard</h1>
+    </div>
+)
+
+
+export default Homepage;
